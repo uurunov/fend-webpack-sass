fcd746da7596add973199e99a0d8149385099c99
uurunov <ulugbekurunov1997@mail.ru>
2020-09-16 16:20:48 +0500

    initial change

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,5 +1,11 @@
 function handleSubmit(event) {
-    event.preventDefault()
+	event.preventDefault()
+
+	/* Global Variables */
+	const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+
+	// Personal API Key for OpenWeatherMap API
+	const keyAPI= '&appid=a7676115f0cff66c4c21188403d28750&units=imperial';
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
@@ -9,8 +15,16 @@ function handleSubmit(event) {
     fetch('http://localhost:8081/test')
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+		document.getElementById('results').innerHTML = res.message
+	})
+
+	// get the zip code input
+	const userZipCode = document.getElementById('zip').value
+	fetch(baseURL + userZipCode + keyAPI)
+	.then(res => res.json())
+	.then(function(res) {
+		document.getElementById('temperature').innerHTML = res.main.temp
+	})
 }
 
 export { handleSubmit }
